e4340875c47b220e2edd329ca0758e7f1a9514e3
Prateek Jayaswal <jayaswalprateek@gmail.com>
2024-07-05 22:29:55 +0530

    feat: add EventListener for container

--- 5-etch-a-sketch/script.js
+++ 5-etch-a-sketch/script.js
@@ -2,13 +2,16 @@
 const gridContainer = document.getElementById("container");
 for (let i = 0; i < 16; i++){
     const row = document.createElement("div");
-    row.id = "r"+i;
     for (let j = 0; j < 16; j++){
         const col = document.createElement("div");
-        col.id = "c" + j;
         col.className = "square";
         row.appendChild(col);
     }
     row.style.display = "flex";
     gridContainer.appendChild(row);
 }
+console.log(document.getElementById("container"));
+document.getElementById("container").addEventListener("mouseover", (e) => {
+    if (e.target.id === "container") return;
+    e.target.style.backgroundColor = "black";
+});
