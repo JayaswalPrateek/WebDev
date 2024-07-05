5ba8ab373b5dde0e83eb16868b64e31dfa09eb87
Prateek Jayaswal <jayaswalprateek@gmail.com>
2024-07-06 01:35:05 +0530

    feat: support dynamic grid resizing

--- 5-etch-a-sketch/script.js
+++ 5-etch-a-sketch/script.js
@@ -1,17 +1,31 @@
 'use strict';
-const gridContainer = document.getElementById("container");
-for (let i = 0; i < 16; i++){
-    const row = document.createElement("div");
-    for (let j = 0; j < 16; j++){
-        const col = document.createElement("div");
-        col.className = "square";
-        row.appendChild(col);
+function buildGrid(dimension) {
+    const gridContainer = document.getElementById("container");
+    for (let i = 0; i < dimension; i++){
+        const row = document.createElement("div");
+        for (let j = 0; j < dimension; j++){
+            const col = document.createElement("div");
+            col.className = "square";
+            row.appendChild(col);
+        }
+        row.style.display = "flex";
+        gridContainer.appendChild(row);
     }
-    row.style.display = "flex";
-    gridContainer.appendChild(row);
 }
-console.log(document.getElementById("container"));
 document.getElementById("container").addEventListener("mouseover", (e) => {
     if (e.target.id === "container") return;
     e.target.style.backgroundColor = "black";
 });
+document.getElementById("configure").addEventListener("click", (e) => {
+    const newDimension = Number(prompt("Enter New Dimension: "));
+    if (newDimension < 1 || newDimension > 100) {
+        alert("Invalid Dimension(Range: 1-100)");
+        return;
+    }
+    const gridContainer = document.getElementById("container");
+    while (gridContainer.firstChild) {
+        gridContainer.removeChild(gridContainer.firstChild);
+    }
+    buildGrid(newDimension);
+});
+buildGrid(16);
