ecef5064e2dd54d5d97d0006e7a0cc71ea2079d9
Prateek Jayaswal <jayaswalprateek@gmail.com>
2024-09-13 00:15:08 +0530

    style: ESLint existing files

--- 5-etch-a-sketch/script.js
+++ 5-etch-a-sketch/script.js
@@ -1,43 +1,43 @@
-'use strict';
+"use strict";
 function buildGrid(dimension) {
-    const gridContainer = document.getElementById("container");
-    for (let i = 0; i < dimension; i++){
-        const row = document.createElement("div");
-        for (let j = 0; j < dimension; j++){
-            const col = document.createElement("div");
-            col.className = "square";
-            row.appendChild(col);
-        }
-        row.style.display = "flex";
-        gridContainer.appendChild(row);
+  const gridContainer = document.getElementById("container");
+  for (let i = 0; i < dimension; i++) {
+    const row = document.createElement("div");
+    for (let j = 0; j < dimension; j++) {
+      const col = document.createElement("div");
+      col.className = "square";
+      row.appendChild(col);
     }
+    row.style.display = "flex";
+    gridContainer.appendChild(row);
+  }
 }
 document.getElementById("container").addEventListener("mouseover", (e) => {
-    if (e.target.id === "container") return;
-    if (!e.target.style.backgroundColor || !e.target.dataset.brightnessLevel) {
-        const x = Math.floor(Math.random() * 256);
-        const y = Math.floor(Math.random() * 256);
-        const z = Math.floor(Math.random() * 256);
-        e.target.style.backgroundColor = `rgb(${x}, ${y}, ${z})`;
-        e.target.dataset.brightnessLevel = 1;
-    } else {
-        let brightnessLevel = parseFloat(e.target.dataset.brightnessLevel);
-        brightnessLevel -= 0.1;
-        if (brightnessLevel < 0) brightnessLevel = 0;
-        e.target.dataset.brightnessLevel = brightnessLevel;
-        e.target.style.filter = `brightness(${brightnessLevel})`;
-    }
+  if (e.target.id === "container") return;
+  if (!e.target.style.backgroundColor || !e.target.dataset.brightnessLevel) {
+    const x = Math.floor(Math.random() * 256);
+    const y = Math.floor(Math.random() * 256);
+    const z = Math.floor(Math.random() * 256);
+    e.target.style.backgroundColor = `rgb(${x}, ${y}, ${z})`;
+    e.target.dataset.brightnessLevel = 1;
+  } else {
+    let brightnessLevel = parseFloat(e.target.dataset.brightnessLevel);
+    brightnessLevel -= 0.1;
+    if (brightnessLevel < 0) brightnessLevel = 0;
+    e.target.dataset.brightnessLevel = brightnessLevel;
+    e.target.style.filter = `brightness(${brightnessLevel})`;
+  }
 });
 document.getElementById("configure").addEventListener("click", (e) => {
-    const newDimension = Number(prompt("Enter New Dimension: "));
-    if (newDimension < 1 || newDimension > 100) {
-        alert("Invalid Dimension(Range: 1-100)");
-        return;
-    }
-    const gridContainer = document.getElementById("container");
-    while (gridContainer.firstChild) {
-        gridContainer.removeChild(gridContainer.firstChild);
-    }
-    buildGrid(newDimension);
+  const newDimension = Number(prompt("Enter New Dimension: "));
+  if (newDimension < 1 || newDimension > 100) {
+    alert("Invalid Dimension(Range: 1-100)");
+    return;
+  }
+  const gridContainer = document.getElementById("container");
+  while (gridContainer.firstChild) {
+    gridContainer.removeChild(gridContainer.firstChild);
+  }
+  buildGrid(newDimension);
 });
 buildGrid(16);
