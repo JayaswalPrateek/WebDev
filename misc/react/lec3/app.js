382f4c551f46b598eed3a6e3849b7a06ab280efe
Prateek Jayaswal <jayaswalprateek@gmail.com>
2024-12-15 14:08:05 +0530

    feat: add JSX and CSS

--- misc/react/lec3/app.js
+++ misc/react/lec3/app.js
@@ -1,13 +1,24 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const parent = React.createElement("div", { id: "parent" },
-    React.createElement("div", { id: "child" }, [
-        React.createElement("h1", { id: "heading1" }, "I'm a h1 sibling"),
-        React.createElement("h2", { id: "heading2" }, "I'm a h2 sibling"),
-    ])
-)
-console.dir(ReactDOM)
+// const parent = React.createElement("div", { id: "parent" },
+//     React.createElement("div", { id: "child" }, [
+//         React.createElement("h1", { id: "heading1" }, "I'm a h1 sibling"),
+//         React.createElement("h2", { id: "heading2" }, "I'm a h2 sibling"),
+//     ])
+// )
+
+// creating the same react element using JSX syntax instead of createElement():
+const parent = <div id="parent">
+    <div id="child">
+        <h1 id="heading1">I'm a h1 sibling</h1>
+        <h2 id="heading2">I'm a h2 sibling</h2>
+    </div>
+</div>
+// creates the same react element as React.createElement() did,
+// but it looks much cleaner than it especially as nesting increases.
+// JSX is not a part of ecmascript spec so parcel bundler transpiles it.
+// Parcel and other bundlers depend on Babel for this traspilation step.
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(parent); // Error: Each child in a list should have a unique "key" prop.
-console.log(parent);
