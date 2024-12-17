9f134dc0b7a97c7d85d6649d690c7eaaf1aaf758
Prateek Jayaswal <jayaswalprateek@gmail.com>
2024-12-17 14:56:19 +0530

    feat: persist header/footer across routes using children routes w/ <Outlet/>

--- react/lec4,5,6,7/src/App.js
+++ react/lec4,5,6,7/src/App.js
@@ -6,29 +6,63 @@ import AppFooter from "./components/AppFooter";
 import AboutUS from "./components/AboutUs";
 import ContactUs from "./components/ContactUS";
 import SmthWentWrong from "./components/SmthWentWrong";
-import { createBrowserRouter, RouterProvider } from "react-router";
+import { createBrowserRouter, RouterProvider, Outlet } from "react-router";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+// const AppContainer = () => (
+//     <div id="appContainer">
+//         <AppHeader />
+//         <AppMain />
+//         <AppFooter />
+//     </div >
+// );
+
+// const appRouter = createBrowserRouter([{
+//     path: "/",
+//     element: <AppContainer />,
+//     errorElement: <SmthWentWrong />,
+// }, {
+//     path: "/about",
+//     element: <AboutUS />,
+// }, {
+//     path: "contactUs",
+//     element: <ContactUs />,
+// }
+// ]);
+
+// root.render(<AppContainer />); without routing
+// root.render(<RouterProvider router={appRouter} />); with routing
+
+// Problem with this approach:
+// the header and footer arent sticky across pages
+// to make them persis across pages we need to us children routes
 
 const AppContainer = () => (
     <div id="appContainer">
-        <AppHeader />
-        <AppMain />
-        <AppFooter />
+        <AppHeader /> {/* always visible */}
+        {/* Conditionally render children here based on the route */}
+        <Outlet /> { /* Just add this and it will take care of it */}
+        <AppFooter /> {/* always visible */}
     </div >
 );
 
 const appRouter = createBrowserRouter([{
     path: "/",
     element: <AppContainer />,
+    children: [
+        {
+            path: "/",
+            element: <AppMain />,
+        }, {
+            path: "/about",
+            element: <AboutUS />,
+        }, {
+            path: "contactUs",
+            element: <ContactUs />,
+        }],
     errorElement: <SmthWentWrong />,
-}, {
-    path: "/about",
-    element: <AboutUS />,
-}, {
-    path: "contactUs",
-    element: <ContactUs />,
 }
 ]);
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-// root.render(<AppContainer />);
 root.render(<RouterProvider router={appRouter} />);
