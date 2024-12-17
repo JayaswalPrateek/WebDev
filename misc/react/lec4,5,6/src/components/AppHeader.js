a8fadd7996e3eb97a717e9720eabac1f33d54485
Prateek Jayaswal <jayaswalprateek@gmail.com>
2024-12-17 10:21:09 +0530

    feat: add Login/Logout toggle

--- misc/react/lec4,5,6/src/components/AppHeader.js
+++ misc/react/lec4,5,6/src/components/AppHeader.js
@@ -1,14 +1,22 @@
-const AppHeader = () => (
-    <header id="appHeader">
-        <div id="logo" className="jua-regular">🥣 Spoonful</div>
-        <nav id="navContainer" className="inter-bold">
-            <ul id="navItems">
-                <li>Home</li>
-                <li>About Us</li>
-                <li>Contact Us</li>
-                <li>Cart</li>
-            </ul>
-        </nav>
-    </header>
-);
+import { useState } from "react";
+
+const AppHeader = () => {
+    const [loginButtonText, setLoginButtonText] = useState("Login")
+    return (
+        <header id="appHeader">
+            <div id="logo" className="jua-regular">🥣 Spoonful</div>
+            <nav id="navContainer" className="inter-bold">
+                <ul id="navItems">
+                    <li>Home</li>
+                    <li>About Us</li>
+                    <li>Contact Us</li>
+                    <li>Cart</li>
+                </ul>
+                <button id="loginButton" className="inter-bold" onClick={() => setLoginButtonText(loginButtonText == 'Login' ? 'Log Out' : 'Login')}>
+                    {loginButtonText}
+                </button>
+            </nav>
+        </header>
+    )
+};
 export default AppHeader;
