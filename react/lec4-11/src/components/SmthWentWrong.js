e1c3390e34702df6f677ef2367e19291d64bbc6f
Prateek Jayaswal <jayaswalprateek@gmail.com>
2024-12-24 23:56:43 +0530

    fix(Error Page): add Tailwind CSS to the error page

--- react/lec4-11/src/components/SmthWentWrong.js
+++ react/lec4-11/src/components/SmthWentWrong.js
@@ -5,8 +5,8 @@ const SmthWentWrong = () => {
     console.log(error);
     return (
         <div>
-            <h1>OOPS! Something Went Wrong!</h1>
-            <h3>{error.data}</h3>
+            <h1 className="text-8xl font-bold p-5">OOPS! Something Went Wrong!</h1>
+            <h3 className="text-4xl italic p-5">{error.data}</h3>
         </div>
     )
 };
