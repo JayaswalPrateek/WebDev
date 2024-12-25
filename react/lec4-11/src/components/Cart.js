ae1d58eb480625ba623c190c986564cd36d8099f
Prateek Jayaswal <jayaswalprateek@gmail.com>
2024-12-26 00:14:33 +0530

    feat(cart): add clear cart button w/ redux

--- react/lec4-11/src/components/Cart.js
+++ react/lec4-11/src/components/Cart.js
@@ -2,6 +2,8 @@ import { useSelector } from "react-redux";
 import AccordianList from "./AccordianList";
 import { useContext } from "react";
 import UserContext from "../utils/UserContext";
+import { useDispatch } from "react-redux"
+import { clearCart } from "../utils/cartSlice";
 
 const Cart = () => {
     const cart = useSelector((store) => store.cart.items)
@@ -19,11 +21,22 @@ const Cart = () => {
 
     const { loggedInUsername } = useContext(UserContext);
 
+    const dispatch = useDispatch() // Dispatch an action using useDispatch()
+
     return (
         <div id="cart" className="text-center m-1 p-10">
             {
                 (cart.length != 0) ?
                     <div className="w-6/12 mx-auto my-4 bg-[lightgrey] shadow-lg p-4">
+                        <div className="text-right">
+                            <button className="px-4 py-2 bg-red-500 text-white rounded-lg hover:bg-red-600 font-bold flex items-center gap-2 ml-auto"
+                                onClick={() => {
+                                    // dispatch an action that was exported by the slice
+                                    dispatch(clearCart())
+                                    // action is the argument to the dispatcher
+                                    // no argument to this action ie no payload
+                                }}> Clear Cart </button>
+                        </div>
                         <AccordianList data={data} />
                         <div className="flex justify justify-between">
                             <h1 className="text-4xl font-bold text-left pl-6">Total</h1>
