d915ce9a4c8afced3621d5d7d6ff19f103907599
Prateek Jayaswal <jayaswalprateek@gmail.com>
2025-01-07 19:45:16 +0530

    feat: add helper utility to save json file

---
+++ backend/lec0/vanillaNodeRestApi/utils/commitDataToFile.js
@@ -0,0 +1,8 @@
+import fs from "fs"
+
+export default function commitDataToFile(fileName, data) {
+    fs.writeFileSync(fileName, JSON.stringify(data), "utf8", (err) => {
+        return { success: false, error: err }
+    });
+    return { success: true, error: null }
+}
