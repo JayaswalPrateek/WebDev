e72219391527c743d87027f95c11fc3f31b25b9d
Prateek Jayaswal <jayaswalprateek@gmail.com>
2025-03-28 16:03:24 +0530

    feat(routing): add edit route handling

--- golang/writingWebApps/main.go
+++ golang/writingWebApps/main.go
@@ -2,18 +2,19 @@ package main
 
 import (
 	"fmt"
+	"html/template"
 	"log"
 	"net/http"
 	"os"
 )
 
 type Page struct {
-	title string
-	body  []byte
+	Title string
+	Body  []byte
 }
 
 func (p *Page) save() error {
-	return os.WriteFile(p.title+".txt", p.body, 0600)
+	return os.WriteFile(p.Title+".txt", p.Body, 0600)
 }
 
 func loadPage(title string) (*Page, error) {
@@ -33,15 +34,36 @@ func viewHandler(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, err.Error(), http.StatusNotFound)
 		return
 	}
-	body := string(p.body)
+	body := string(p.Body)
 	fmt.Printf("Read: '%s'\n", body)
 	fmt.Fprintf(w, `
 		<h1>%s</h1>
 		<div><i>%s</i></div>
-	`, p.title, p.body)
+	`, p.Title, p.Body)
+}
+
+func editHandler(w http.ResponseWriter, r *http.Request) {
+	title := r.URL.Path[len("/edit/"):]
+	p, err := loadPage(title)
+
+	if err != nil {
+		p = &Page{title, nil}
+	}
+	t, err := template.ParseFiles("edit.html")
+	if err != nil {
+		fmt.Println("Error:", err)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+	t.Execute(w, p)
+}
+
+func saveHandler(w http.ResponseWriter, r *http.Request) {
+
 }
 
 func main() {
 	http.HandleFunc("/view/", viewHandler)
+	http.HandleFunc("/edit/", editHandler)
+	http.HandleFunc("/save/", saveHandler)
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
