e8f4f2cc284151f4a0ea9a08765c9339769923cc
Prateek Jayaswal <jayaswalprateek@gmail.com>
2025-03-28 12:58:53 +0530

    fix(logging): log and quit upon error while serving

--- golang/diyServer/main.go
+++ golang/diyServer/main.go
@@ -91,5 +91,5 @@ func main() {
 	mux.HandleFunc("GET /user/{id}", h.getUserByID)
 	mux.HandleFunc("DELETE /user/{id}", h.deleteUserByID)
 	fmt.Println("Server listening to :8080 on localhost")
-	http.ListenAndServe(":8080", mux)
+	log.Fatal(http.ListenAndServe(":8080", mux))
 }
